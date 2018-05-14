25f3e45f749f7c27b6394a5b3ab95a092a5ea0f6
Felice Forby <feliceforby@gmail.com>
2018-05-14 21:52:32 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+  attr_reader :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(piece_of_knowledge)
+    @knowledge << piece_of_knowledge
+  end
 end
